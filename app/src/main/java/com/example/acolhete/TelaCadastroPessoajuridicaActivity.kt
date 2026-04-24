9e8a1703447dd6084103b6096dd8455538b053de
Tainá Kasai <talitasabine2@gmail.com>
2026-04-24 11:40:05 -0300

    Feat: Adiciona tela de cadastro juridicoi

--- app/src/main/java/com/example/acolhete/TelaCadastroPessoajuridicaActivity.kt
+++ app/src/main/java/com/example/acolhete/TelaCadastroPessoajuridicaActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class TelaCadastroActivity : AppCompatActivity() {
+class TelaCadastroPessoajuridicaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tela_cadastro_pessoafisica)
+        setContentView(R.layout.activity_tela_cadastro_pessoajuridica)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
